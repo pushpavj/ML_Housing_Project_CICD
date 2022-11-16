4f9f4d9f570f9a7f2f470e88de4d9f0166e26c05
PushpaVJ <pushpa_vj@rediffmail.com>
2022-11-17 02:20:16 +0530

    project structure updated

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys 
+
 
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception('We are testing custom exception ')
+    except Exception as e:
+        housing= HousingException(e,sys)
+      #  print(housing.__str__())
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module ") #Just to test if the log module works fine
     return "Starting ML-Housing project"
 
 if __name__=="__main__":
