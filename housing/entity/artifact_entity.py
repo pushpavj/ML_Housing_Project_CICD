c5dc89de8ca6b65669396c096da6468bea97faba
PushpaVJ <pushpa_vj@rediffmail.com>
2022-11-19 00:38:28 +0530

    dataingestion component is coded

---
+++ housing/entity/artifact_entity.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+
+DataIngestionArtifact=namedtuple('DataIngestionArtifact',["train_file_path",
+"test_file_path","is_ingested","message"])
+
+#["train_file_path", location of the training data set 
+#"test_file_path",   locoation of the test data set
+# "is_ingested",     whether the data ingested successfully or not
+# "message"])        Some messages we can pass
